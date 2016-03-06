60959012ac2722d5e27bb6513f207ed0a1a4cdbd
Anthony Camboni <antca@users.noreply.github.com>
2016-03-06 20:06:20 +0100

    Move hot handler in routes

--- src/server/index.js
+++ src/server/index.js
@@ -3,23 +3,16 @@ import path from 'path';
 import Koa from 'koa';
 import koaSend from 'koa-send';
 
-const routes = require('./routes').default;
+import routes from './routes';
 
 const app = new Koa();
 
-let allRoutes = routes.routes();
-
 if(__DEV__) {
   app.use(require('./middlewares/dev').default());
-  if(module.hot) {
-    module.hot.accept('./routes', () => {
-      allRoutes = require('./routes').default.routes();
-    });
-  }
 }
 
 app
-.use(async (ctx, next) => allRoutes(ctx, next))
+.use(routes.routes())
 .use(async (ctx) => {
   await koaSend(ctx, ctx.path, { root: path.resolve(__dirname, 'public') });
 })
--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,10 +1,20 @@
 import Router from 'koa-router';
 
-import render from './render';
-import api from './api';
+const render = require('./render').default;
+const api = require('./api').default;
 
 const routes = new Router()
 .use('/', render.routes())
 .use('/api', api.routes());
+
+
+if(module.hot) {
+  module.hot.accept(['./render', './api'], () => {
+    routes.stack = [];
+    routes.use(require('./api').default.routes());
+    routes.use(require('./render').default.routes());
+  });
+}
+
 
 export default routes;
