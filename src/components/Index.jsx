83e2833a8bd752dcac22d0e34088c2cf2e65d384
Anthony Camboni <antca@users.noreply.github.com>
2016-03-13 16:51:29 +0100

    Dry up render route

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import { js2b64 } from '../util/b64';
 
-function Index({ title, flux, markup, cssPath, jsPath }) {
+function Index({ ctx, flux, markup }) {
   return (
     <html lang='en'>
-    <head>
-      <meta charSet='UTF-8' />
-      <title>{title}</title>
-      {cssPath ? <link rel='stylesheet' href={cssPath} /> : null}
-    </head>
-    <body>
-      <div id='app' data-flux={flux} dangerouslySetInnerHTML={{ __html: markup }} />
-      <script src={jsPath}></script>
-    </body>
+      <head>
+        <meta charSet='UTF-8' />
+        <title>{'React Nexus App'}</title>
+        {__DEV__ ? null : <link rel='stylesheet' href={'public/client.css'} /> }
+      </head>
+      <body>
+        <div id='app' data-flux={js2b64(flux.dumpState())} dangerouslySetInnerHTML={{ __html: markup }} />
+        <script src={'public/client.js'}></script>
+      </body>
     </html>
   );
 }
--- src/server/routes/render/index.js
+++ src/server/routes/render/index.js
@@ -3,22 +3,19 @@ import ReactDOM from 'react-dom/server';
 import Router from 'koa-router';
 import { prepare } from 'react-nexus';
 
-import { js2b64 } from '../../../util/b64';
 import createFlux from '../../../nexus/createFlux';
 import App from '../../../components/App';
 import Index from '../../../components/Index';
 
 const render = new Router()
 .get('/', async (ctx) => {
-  const flux = createFlux();
+  const flux = createFlux(ctx);
   const app = React.createElement(App, { flux });
   await prepare(app);
   const index = React.createElement(Index, {
-    flux: js2b64(flux.dumpState()),
+    ctx,
+    flux,
     markup: ReactDOM.renderToString(app),
-    cssPath: __DEV__ ? null : 'public/client.css',
-    jsPath: 'public/client.js',
-    title: 'App title',
   });
   ctx.body = `<!DOCTYPE html>${ReactDOM.renderToStaticMarkup(index)}`;
 });
