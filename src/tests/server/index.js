b3804a8d67915cc7c61e665a12f69424a039f865
Anthony Camboni <antca@users.noreply.github.com>
2016-03-14 22:43:50 +0100

    Empty test files

--- src/tests/server/index.js
+++
@@ -1,9 +0,0 @@
-import { Test } from '../../components/App/Test';
-
-describe('Components', () => {
-  describe('TestMessage', () => {
-    it('should render correctly', () => {
-
-    });
-  });
-});
