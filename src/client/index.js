0db96dbc0cfc5dcb55c0b2d68bb4c7db3c7ec497
Anthony Camboni <antca@users.noreply.github.com>
2016-10-22 14:15:02 +0200

    Add simple shader system and remove unused code

--- src/client/index.js
+++ src/client/index.js
@@ -30,7 +30,7 @@ const { abs } = Math
 async function loadModel(url) {
   const response = await fetch(url)
   const modelText = await response.text()
-  return modelText.split('\n').reduce((m, line) => {
+  const model = modelText.split('\n').reduce((m, line) => {
     const [type, ...data] = line.split(/\s+/)
     if (type === 'v') {
       m.vertices.push(data.map(Number))
@@ -41,8 +41,16 @@ async function loadModel(url) {
     if (type === 'f') {
       m.faces.push(data.map((f) => f.split('/').map(Number)))
     }
+    if (type === 'vn') {
+      m.normals.push(data.map(Number))
+    }
     return m
-  }, { vertices: [], uvs: [], faces: [] })
+  }, { vertices: [], uvs: [], faces: [], normals: [] })
+  return model.faces.map((faceData) => {
+    return faceData.map(([v, uv, n]) => {
+      return [model.vertices[v - 1], model.uvs[uv - 1], model.normals[n - 1]]
+    })
+  })
 }
 
 async function loadTexture(url) {
@@ -59,44 +67,6 @@ async function loadTexture(url) {
       resolve(ctx.getImageData(0, 0, canvas.width, canvas.height))
     })
   })
-}
-
-function swap([x, y]) {
-  return [y, x]
-}
-
-function line(a, b, ...color) {
-  const points = []
-  let steep = false
-  if (abs(a[0] - b[0]) < abs(a[1] - b[1])) {
-    a = swap(a)
-    b = swap(b)
-    steep = true
-  }
-  if (a[0] > b[0]) {
-    const tmp = a
-    a = b
-    b = tmp
-  }
-  const dx = b[0] - a[0]
-  const dy = b[1] - a[1]
-  let de = abs(dy) << 1
-  let e = 0
-  let y = a[1]
-  for (let x = a[0]; x <= b[0]; x++) {
-    if (steep) {
-      points.push([y, x, ...color])
-    }
-    else {
-      points.push([x, y, ...color])
-    }
-    e += de
-    if (e > dx) {
-      y += (b[1] > a[1] ? 1 : -1)
-      e -= dx << 1
-    }
-  }
-  return points
 }
 
 function computeBB(points) {
@@ -118,30 +88,16 @@ function computeBB(points) {
 }
 
 function plot(points) {
-  const bb = computeBB(points)
-  const width = abs(bb[1][0] - bb[0][0]) + 1
-  const height = abs(bb[1][1] - bb[0][1]) + 1
-  if (width === 0 || height === 0) {
-    return
-  }
-  const imageData = ctx.getImageData(bb[0][0], bb[0][1], width, height)
+  const imageData = ctx.getImageData(0, 0, width, height)
   for (let i = 0; i < points.length; ++i) {
     const d = points[i]
-    const pixelIndex = (((d[0] - bb[0][0]) * 4 + (d[1] - bb[0][1]) * width * 4))
+    const pixelIndex = (((d[0] - 0) * 4 + (d[1] - 0) * width * 4))
     imageData.data[pixelIndex] = d[2]
     imageData.data[pixelIndex + 1] = d[3]
     imageData.data[pixelIndex + 2] = d[4]
     imageData.data[pixelIndex + 3] = d[5]
   }
-  ctx.putImageData(imageData, bb[0][0], bb[0][1])
-}
-
-function triangle(a, b, c, ...color) {
-  return [
-    ...line(a, b, ...color),
-    ...line(b, c, ...color),
-    ...line(c, a, ...color)
-  ]
+  ctx.putImageData(imageData, 0, 0)
 }
 
 function cross(a, b) {
@@ -171,14 +127,6 @@ function normalize(a) {
 
 function scale(a, s) {
   return [a[0] * s, a[1] * s, a[2] * s]
-}
-
-function add(a, v) {
-  return [a[0] + v, a[1] + v, a[2] + v]
-}
-
-function sum(a, b) {
-  return [a[0] + b[0], a[1] + b[1], a[2] + b[2]]
 }
 
 function mul(a, b) {
@@ -279,28 +227,25 @@ function viewport(x, y, w, h) {
   return m
 }
 
-function fillTriangle(zbuffer, texture, intensity, a, b, c, uva, uvb, uvc) {
+function triangle(zbuffer, shader, face) {
+  const va = face[0].map(v => v | 0)
+  const vb = face[1].map(v => v | 0)
+  const vc = face[2].map(v => v | 0)
   const points = []
-  const [bbmin, bbmax] = computeBB([a, b, c])
+  const [bbmin, bbmax] = computeBB([va, vb, vc])
   for (let x = bbmin[0]; x <= bbmax[0]; x++) {
     for (let y = bbmin[1]; y <= bbmax[1]; y++) {
-      const [bca, bcb, bcc] = barycentric(a, b, c, [x, y])
+      const bar = barycentric(va, vb, vc, [x, y])
+      const [bca, bcb, bcc] = bar
       if (bca >= 0 && bcb >= 0 && bcc >= 0) {
-        const z = a[2] * bca + b[2] * bcb + c[2] * bcc
+        const z = va[2] * bca + vb[2] * bcb + vc[2] * bcc
         const zBufferValue = zbuffer[x + y * width]
         if (zBufferValue === void 0 || zBufferValue < z) {
-          const puv = [scale(uva, bca), scale(uvb, bcb), scale(uvc, bcc)].reduce(sum)
-          const tc = mul(puv, [texture.width, texture.height]).map((v) => v | 0)
-          const bi = (tc[0] * 4 + tc[1] * 4 * texture.width)
           zbuffer[x + y * width] = z
-          points.push([
-            x,
-            y,
-            texture.data[bi] * intensity,
-            texture.data[bi + 1] * intensity,
-            texture.data[bi + 2] * intensity,
-            texture.data[bi + 3],
-          ])
+          const color = shader(bar)
+          if(color) {
+            points.push([x, y, ...color])
+          }
         }
       }
     }
@@ -308,55 +253,86 @@ function fillTriangle(zbuffer, texture, intensity, a, b, c, uva, uvb, uvc) {
   return points
 }
 
-const viewportMatrix = viewport(0, 0, width, height)
-const lookAtMatrix = lookAt(
-  [0.5, 0, 1],
-  [0, 0.2, 0],
-  [0, 1, 0],
-)
-const projectionMatrix = [
-  [0.5, 0.0, 0.0, 0.0],
-  [0.0, 0.5, 0.0, 0.0],
-  [0.0, 0.0, 0.5, 0.0],
-  [0.0, 0.0, -0.2, 1.0],
-]
-const matrix = mulM(viewportMatrix, mulM(lookAtMatrix, projectionMatrix))
-
-function renderWireframe(model) {
-  model.faces.map((f) => {
-    const [a, b, c] = f.map(([v]) => {
-      return model.vertices[v - 1]
-    })
-    const ta = toCart(applyMatrix([...a, 1], matrix)).map((v) => v | 0)
-    const tb = toCart(applyMatrix([...b, 1], matrix)).map((v) => v | 0)
-    const tc = toCart(applyMatrix([...c, 1], matrix)).map((v) => v | 0)
-    plot(triangle(ta, tb, tc, 255, 255, 255, 255))
-  })
+function getPixel(texture, uv) {
+  const tc = mul(uv, [texture.width, texture.height]).map((v) => v | 0)
+  const bi = (tc[0] * 4 + tc[1] * 4 * texture.width)
+  return [
+    texture.data[bi],
+    texture.data[bi + 1],
+    texture.data[bi + 2],
+    texture.data[bi + 3],
+  ]
 }
 
-function renderWithLight(model, texture, light) {
-  const zbuffer = new Array(width * height)
-  model.faces.map((f) => {
-    const [[a, uva], [b, uvb], [c, uvc]] = f.map(([v, uv]) => {
-      return [model.vertices[v - 1], model.uvs[uv - 1]]
-    })
-    const normal = normalize(cross(sub(c, a), sub(b, a)))
-    const intensity = mul(normal, light)[2]
-    const ta = toCart(applyMatrix([...a, 1], matrix)).map((v) => v | 0)
-    const tb = toCart(applyMatrix([...b, 1], matrix)).map((v) => v | 0)
-    const tc = toCart(applyMatrix([...c, 1], matrix)).map((v) => v | 0)
-    if(intensity > 0) {
-      plot(fillTriangle(zbuffer, texture, intensity, ta, tb, tc, uva, uvb, uvc))
+function gouraudShader(matrix, texture, light) {
+  let varying_intensity = [0, 0, 0]
+  let varying_uv = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+  ]
+  return {
+    vertex([v, uv, n], i) {
+      varying_uv[0][i] = uv[0]
+      varying_uv[1][i] = uv[1]
+      varying_intensity[i] = Math.max(0, mul(n, light).reduce((sum, x) => sum + x))
+      return applyMatrix([...v, 1], matrix)
+    },
+    fragment(bc) {
+      const intensity = mul(varying_intensity, bc).reduce((sum, x) => sum + x)
+      const uv = applyMatrix([...bc, 1], varying_uv)
+      const color = getPixel(texture, uv)
+      return [...scale(color, intensity), 255]
     }
-  })
+  }
+}
+
+function cheapWireframeShader(matrix) {
+  return {
+    vertex([v, uv, n], i) {
+      return applyMatrix([...v, 1], matrix)
+    },
+    fragment(bc) {
+      if(bc[0] < 0.03 || bc[1] < 0.03 || bc[2] < 0.03) {
+        return [255, 255, 255, 255]
+      }
+      return null
+    }
+  }
+}
+
+function render(model, shader) {
+  const zbuffer = new Array(width * height)
+  const points = []
+  model.forEach((f) => {
+    return points.push(...triangle(zbuffer, shader.fragment, f.map(shader.vertex).map(toCart)))
+  }, [])
+  plot(points)
 }
 
 async function main() {
   const model = await loadModel('https://raw.githubusercontent.com/ssloy/tinyrenderer/master/obj/african_head/african_head.obj')
   const texture = await loadTexture('http://i.imgur.com/jGAm5aP.jpg')
+  const light = [0.5, 0.5, 1.0]
+  const viewportMatrix = viewport(0, 0, width, height)
+  const lookAtMatrix = lookAt(
+    [0.6, 0.2, 1],
+    [0, 0, 0],
+    [0, 1, 0],
+  )
+  const projectionMatrix = [
+    [0.6, 0.0, 0.0, 0.1],
+    [0.0, 0.6, 0.0, 0.1],
+    [0.0, 0.0, 0.6, 0.1],
+    [0.0, 0.0, -0.2, 1.0],
+  ]
+  const matrix = mulM(viewportMatrix, mulM(lookAtMatrix, projectionMatrix))
+  const gouraud = gouraudShader(matrix, texture, light)
+  const wireframe = cheapWireframeShader(matrix)
   ctx.fillRect(0, 0, width, height)
-  renderWithLight(model, texture, [0, 0, -1])
-  renderWireframe(model)
+  render(model, gouraud)
+  //render(model, wireframe)
 }
 
 main()
